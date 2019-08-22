d1425c7deb008ca01a41464a05179859079c87b8
Paco Luna <pakoo_15@hotmail.com>
2019-08-22 18:27:12 -0500

    Create relation user with lists
    Modify user and list models to create the relation and make a new
    migration to generate a foreign key to lists database.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
+  belongs_to :user
   has_many :tasks
   validates :title, presence: true,
                     length: { minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_224513) do
+ActiveRecord::Schema.define(version: 2019_08_22_232116) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
