ab8672f2d3af19c57fb9408a3305df8d487c7140
Paco Luna <pakoo_15@hotmail.com>
2019-08-28 17:13:35 -0500

    Fix update users
    Change controller application to allow "name, description and password"
    fields in the sign in, sign up and account update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :description, :email, :password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :description, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :description, :email, :password, :current_password])
+  end
 end
